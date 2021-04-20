47168a4351c6e7d4b34965d1f5678afe4fe8e524
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2021-04-20 12:31:17 +0200

    Added new tests

--- src/main/java/DBAppException.java
+++ src/main/java/DBAppException.java
@@ -1,30 +1,9 @@
 public class DBAppException extends Exception{
-    /**
-     * Constructs a new exception with {@code null} as its detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause}.
-     */
     public DBAppException() {
+        super();
     }
 
-    /**
-     * Constructs a new exception with the specified detail message.  The
-     * cause is not initialized, and may subsequently be initialized by
-     * a call to {@link #initCause}.
-     *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
-     */
     public DBAppException(String message) {
         super(message);
-    }
-
-
-    public DBAppException(String s) {
-        super(s);
-    }
-
-    public DBAppException() {
-
     }
 }
