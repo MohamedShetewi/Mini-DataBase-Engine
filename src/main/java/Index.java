6cab1086f00191678413811efabd92b59cd495e5
someone <mohammedalnamaki@gmail.com>
2021-06-03 22:14:50 +0200

    get buckets

--- src/main/java/Index.java
+++ src/main/java/Index.java
@@ -112,22 +112,7 @@ public class Index implements Serializable {
         return columnNames;
     }
 
-    static class Range implements Serializable {
-        Comparable minVal, maxVal;
 
-        public Range(Comparable minVal, Comparable maxVal) {
-            this.minVal = minVal;
-            this.maxVal = maxVal;
-        }
-
-        @Override
-        public String toString() {
-            return "Range{" +
-                    "minVal=" + minVal +
-                    ", maxVal=" + maxVal +
-                    '}';
-        }
-    }
 
 
 }
