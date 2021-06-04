a6e90c8250a6906506b53fe716656d5a1a0c2025
Ahmed hossam <toxin435@gmail.com>
2021-06-04 22:07:28 +0200

    Modified DeletFromTable()

--- src/main/java/RowReference.java
+++ src/main/java/RowReference.java
@@ -1,4 +1,5 @@
 import java.io.Serializable;
+import java.util.Objects;
 
 public class RowReference implements Serializable {
 
@@ -17,5 +18,18 @@ public class RowReference implements Serializable {
 
     public Object getClusteringValue() {
         return clusteringValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RowReference that = (RowReference) o;
+        return Objects.equals(pagePath, that.pagePath) && Objects.equals(clusteringValue, that.clusteringValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pagePath, clusteringValue);
     }
 }
